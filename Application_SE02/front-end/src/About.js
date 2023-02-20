ce46584b2080d9ed8e48a9a80fa0c15fa2f57518
Ashmitha <ashdale30@gmail.com>
2023-02-20 12:45:38 -0800

    Push build

--- Application_SE02/front-end/src/About.js
+++ Application_SE02/front-end/src/About.js
@@ -1,17 +1,25 @@
-import photo from './photo.jpg';
+/* WRITE YOUR PHOTO PATH HERE vvv */
+import ashmitha_photo from './photos/ashmitha.jpg';
+import steve_photo from './photos/photo.jpg';
+import preet_photo from './photos/photo.jpg';
+import chris_photo from './photos/photo.jpg';
+import abdul_photo from './photos/photo.jpg';
+import nathan_photo from './photos/nathan.jpg';
+
 import { useNavigate } from 'react-router-dom';
 
 function About(){
-    
+
     let navigate = useNavigate();
-    
+
     const routeChange = (name) => {
         let path = '/' + name;
         navigate(path);
-    } 
+    }
 
     // List of all current members
     const members = ["Ashmitha Pais", "Steve Betts", "Preet Dhaliwal", "Chris Farnsworth", "Abdul Barrie", "Nathan Loo"];
+    const member_photos = [ashmitha_photo, steve_photo, preet_photo, chris_photo, abdul_photo, nathan_photo];
     const routes = ["ashmitha", "steve", "preet", "chris", "abdul", "nathan"];
 
     // Create a member template for each member in the list above.
@@ -20,11 +28,11 @@ function About(){
             <h1 class="heading">CSC 648 - Software Engineering</h1>
             <h2 class="heading">Section 4</h2>
             <h3 class="heading">Team 02</h3>
-            
+
             <div className='member-container'>
                 {members.map((member, index) => (
                     <div class='member' onClick={() => {routeChange(routes[index])}}>
-                        <img class='member-photo' src={photo} alt={member}/>
+                        <img class='member-photo' src={member_photos[index]} alt={member}/>
                         <p class='member-title'>{member}</p>
                     </div>
                 ))}
--- Application_SE02/front-end/src/App.js
+++ Application_SE02/front-end/src/App.js
@@ -1,4 +1,10 @@
-import photo from './photo.jpg';
+/* WRITE YOUR PHOTO PATH HERE vvv */
+import ashmitha_photo from './photos/ashmitha.jpg';
+import steve_photo from './photos/photo.jpg';
+import preet_photo from './photos/photo.jpg';
+import chris_photo from './photos/photo.jpg';
+import abdul_photo from './photos/photo.jpg';
+import nathan_photo from './photos/nathan.jpg';
 
 import Navbar from './Navbar.js';
 import About from './About.js';
@@ -6,22 +12,32 @@ import Member from './Member.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+
+    let ashmitha_text = "I am Ashmitha Dale Pais, a computer science graduate with a Bachelor's degree from The Oxford College of Engineering in Bangalore, India. Currently, I am pursuing a Master's degree in Computer Science at San Francisco State University. In terms of my academic projects, I have worked on a Blood and Organ Donation Management System using HTML, CSS, PL/SQL, and Apache Tomcat, as well as a Grocery Management System utilizing NodeJS, React frameworks, and Postgres DB.\n" +
+        "\n" +
+        "My technical skills include proficiency in Java, Python, C, PL/SQL, and JavaScript, as well as experience with tools such as Git, Docker, Jira, and Splunk. I have also utilized various libraries such as pandas, numpy, matplotlib, sklearn, seaborn, and scikit for machine learning projects.\n" ;
+    let steve_text = "test2";
+    let preet_text = "test3";
+    let chris_text = "test4";
+    let abdul_text = "test5";
+    let nathan_text = "Hi, my name is Nathan Loo. I'm a Computer Science student at San Francisco State University currently studying for a Bachelor's degree. I was born and raised in San Francisco and from a young age I was fascinated with technology. I've been using computers my whole life so the computer science track seemed right for me. I hope to be able to become a software engineer in the future after completing my degree.";
+
     return (
         <Router>
-            <div className="App"> 
-                
+            <div className="App">
+
                 <Navbar/>
-                
+
                 <div className='content'>
                     <Routes>
                         <Route path='/' element={<About/>}></Route>
                         {/*this is a comment*/}
-                        <Route path='/ashmitha' element={<Member name="Ashmitha Pais" photo={photo}/>}></Route>
-                        <Route path='/steve' element={<Member name="Steve Betts" photo={photo}/>}></Route>
-                        <Route path='/preet' element={<Member name="Preet Dhaliwal" photo={photo}/>}></Route>
-                        <Route path='/chris' element={<Member name="Chris Farnsworth" photo={photo}/>}></Route>
-                        <Route path='/abdul' element={<Member name="Abdul Barrie" photo={photo}/>}></Route>
-                        <Route path='/nathan' element={<Member name="Nathan Loo" photo={photo}/>}></Route>
+                        <Route path='/ashmitha' element={<Member name="Ashmitha Pais" photo={ashmitha_photo} text={ashmitha_text}/>}></Route>
+                        <Route path='/steve' element={<Member name="Steve Betts" photo={steve_photo} text={steve_text}/>}></Route>
+                        <Route path='/preet' element={<Member name="Preet Dhaliwal" photo={preet_photo} text={preet_text}/>}></Route>
+                        <Route path='/chris' element={<Member name="Chris Farnsworth" photo={chris_photo} text={chris_text}/>}></Route>
+                        <Route path='/abdul' element={<Member name="Abdul Barrie" photo={abdul_photo} text={abdul_text}/>}></Route>
+                        <Route path='/nathan' element={<Member name="Nathan Loo" photo={nathan_photo} text={nathan_text}/>}></Route>
                     </Routes>
                 </div>
 
