e33bf8563936d721b5675fe2557e495c48dba3de
Abdul Barrie <asbarrie14@yahoo.com>
2023-02-21 14:57:41 -0800

    Fixed About page by adding bio and pic to About and App.js.

--- Application_SE02/front-end/src/Member.js
+++ Application_SE02/front-end/src/Member.js
@@ -1,16 +1,12 @@
-function Member({name, photo}){
+function Member({name, photo, text}){
     return (
         <div class="info-container">
             <img class="info-photo" src={photo} alt={name}/>
-            
-            <h3 class="heading"> {name} </h3> 
-            
-            <p class="info-text"> 
-                Hi, I'm Abdul Barrie, a Computer Science major currently attending San Francisco State University. 
-                I used to read and write a lot as a child, then I discovered computers and video games and my life went downhill from there.
-                …JK! Jokes aside, I've been interested in computers ever since I was young, which was what led me to want to join the CS 
-                field when I grew up. Outside of coding, I enjoy literature, writing short stories and listening to music.
- 
+
+            <h3 class="heading"> {name} </h3>
+
+            <p class="info-text">
+                { text }
             </p>
         </div>
     );
