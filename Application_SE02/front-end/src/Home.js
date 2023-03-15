7e73fdcb18086b8724ab2b5377835c5fd8283ecb
Nathan Loo <nathanloo4042@gmail.com>
2023-03-15 16:58:37 -0700

    Added a dark mode toggle (experimental)

--- Application_SE02/front-end/src/Home.js
+++ Application_SE02/front-end/src/Home.js
@@ -1,12 +1,27 @@
+import { useState } from 'react'
 import Fetch from "./Fetch.js";
 
 function Home(){
 
     const {data, error} = Fetch("test");
+    const [query, setQuery] = useState('');
 
     return(
         <div>
             <h1 className='heading'>This is the home page!</h1>
+            
+            <div className='home-page-search'>
+                <input 
+                    className="home-page-search-input"
+                    type="text" 
+                    placeholder="Search" 
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button className='home-page-submit'>Submit</button>
+            </div>
+
+            <div className='test-query'>{query}</div>
+            
             { data && <div className='data'>{ data }</div>}
             { error && <div className='error'>Error: { error }<br/>Couldn't fetch data!</div>}
         </div>
--- Application_SE02/front-end/src/Navbar.js
+++ Application_SE02/front-end/src/Navbar.js
@@ -1,6 +1,21 @@
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect} from 'react';
 
 function Navbar(){
+
+    const [theme, setTheme] = useState('light');
+
+    function toggleTheme(){
+        if(theme === 'light')
+            setTheme('dark');
+        else
+            setTheme('light');
+    }
+
+    useEffect(() => {
+        document.body.className=theme;
+    }, [theme]);
+
 
     let navigate = useNavigate();
     
@@ -13,6 +28,8 @@ function Navbar(){
         <nav className='navbar'>
             <button onClick={() => {routeChange('')}} className='navbar-button'>Home</button>
             <button onClick={() => {routeChange('about')}} className='navbar-button' id='about'>About</button>
+            <button onClick={ toggleTheme} className='theme-toggle'>Change Theme!</button>
+
         </nav>
     );
 }
