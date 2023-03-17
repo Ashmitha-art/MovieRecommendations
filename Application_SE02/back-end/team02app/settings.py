627464ec2c3675ca14dd1bbdcb075c50e3c0c1ea
stevenabetts <steven.a.betts@outlook.com>
2023-03-17 15:06:04 -0700

    Updated settings, added serializers and views for vertical prototype

--- Application_SE02/back-end/team02app/settings.py
+++ Application_SE02/back-end/team02app/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,9 +55,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
-ROOT_URLCONF = 'back-end.team02app.urls'
+CORS_ORIGIN_ALLOW_ALL = True
+
+ROOT_URLCONF = 'team02app.urls'
 
 TEMPLATES = [
     {
