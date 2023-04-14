49e079da8fa44f6583dad443d3e39bdfadb647d6
Abdul Barrie <asbarrie14@yahoo.com>
2023-04-13 22:52:21 -0700

    Worked on Post requests for login and signup.

--- Application_SE02/front-end/src/Signup.js
+++ Application_SE02/front-end/src/Signup.js
@@ -1,6 +1,7 @@
-
 //import {useState} from 'react';
 import React, { Component } from 'react';
+
+//const { username, email, password, confirmpassword } = this.state;
 
 class SignUpForm extends Component {
 
@@ -14,6 +15,12 @@ state = {
 }
 
 
+
+//const { username, email, password, confirmpassword } = this.state;
+
+//EEj23`~ull1
+
+
 handleChange = (event) => {
   this.setState({
     [event.target.name]: event.target.value
@@ -21,70 +28,90 @@ handleChange = (event) => {
   
 }
 
+// Post Request
  handleSubmit = (event) => {
-  event.preventDefault()
-  if (this.FormValidation()) {
-   console.log(this.state);  
-  }
-};
+  event.preventDefault();
+  if (this.SignupValidation()) {
+   console.log(this.state.email);  
+   console.log(this.state.username);
+   console.log(this.state.password);  
+   console.log(this.state.confirmpassword);  
 
- FormValidation = () => {
+   const userInfo = {
+    email: this.state.email,
+    username: this.state.username,
+    password: this.state.password,
+    confirmpassword: this.state.confirmpassword,
+   };
+  
+  const userPost = {
+    method: 'POST',
+    body: JSON.stringify(userInfo),
+  };
+
+  fetch('/signup', userPost)
+  .then(res => res.json())
+  .catch(event => console.log(event));
+  };
+
+ }
+ SignupValidation = () => {
   let InfoErrors = {}
-  let ValidInfo = true
+  let ValidAccountInfo = true
 
   if (!this.state.email) {
-      ValidInfo = false
+      ValidAccountInfo = false
       InfoErrors['email'] = 'Please enter a valid email.'
 
   } else if (this.state.email) {
       if (!this.state.email.match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/)) {
-          ValidInfo = false
+          ValidAccountInfo = false
           InfoErrors['email'] = 'Email is not valid (must be ex. x@y.com).'
       }
   }
 
   if (!this.state.username) {
-      ValidInfo = false
+      ValidAccountInfo = false
       InfoErrors['username'] = 'Please enter a valid username.'
   } else if (this.state.username) {
       if (!this.state.username.match(/^[A-Za-z][A-Za-z0-9]+$/)) {
-          ValidInfo = false
+          ValidAccountInfo = false
           InfoErrors['username'] = 'Username must begin with an alphanumeric character.'
       } else {
           if (!this.state.username.match(/^[A-Za-z]\w{2,20}$/)) {
-              ValidInfo = false
+              ValidAccountInfo = false
               InfoErrors['username'] = 'Username must contain at least 3 alphanumeric characters.'
           }
       }
   }
 
   if (!this.state.password) {
-      ValidInfo = false
+      ValidAccountInfo = false
       InfoErrors['password'] = 'Please enter a valid password.'
     } else if (this.state.password) {
       if (!this.state.password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/)) {
-        ValidInfo = false
+        ValidAccountInfo = false
         InfoErrors['password'] = 'Password must be at least 8 characters and contain 1 letter, 1 number and a symbol.'
       }
     }
 
     if (!this.state.confirmpassword) {
-      ValidInfo = false
+      ValidAccountInfo = false
       InfoErrors['confirmpassword'] = 'Please confirm your password.'
     } else if (this.state.confirmpassword) {
         if (this.state.confirmpassword !== this.state.password) {
-          ValidInfo = false
+          ValidAccountInfo = false
           InfoErrors['confirmpassword'] = 'Passwords do not match.'
         }
     }
 
-    this.setState({ InfoErrors })
-    return ValidInfo
-  }
+    this.setState({ InfoErrors });
+    return ValidAccountInfo;
+  };
 
-   render () {
+  render () {
    
-    const { username, email, password, confirmpassword } = this.state;
+   const { username, email, password, confirmpassword } = this.state;
 
     //console.log(InfoErrors);
     return (
@@ -93,7 +120,7 @@ handleChange = (event) => {
                 <div className="SignUp_Container">
                     <div className="SignUp_Container_Border">
                         <label className="SignUp_Email">Email</label> 
-                        <input className="LoginRegisterInputFields" type="email" id="email" placeholder="Email" 
+                        <input className="LoginRegisterInputFields" type="email" id="email" placeholder="Email" required
                         name='email'
                         value={email}
                         onChange = {this.handleChange}
@@ -102,7 +129,7 @@ handleChange = (event) => {
                         <br/>
 
                         <label className="SignUp_Username">Username</label>
-                        <input className="LoginRegisterInputFields" type="text" id="Username" placeholder="Username" 
+                        <input className="LoginRegisterInputFields" type="text" id="Username" placeholder="Username" required
                         name='username'
                         value={username}
                         onChange = {this.handleChange}
@@ -111,7 +138,7 @@ handleChange = (event) => {
                         <br/>
 
                         <label className="SignUp_Password">Password</label> 
-                        <input className="LoginRegisterInputFields" type="password" id="Password" placeholder="Password" 
+                        <input className="LoginRegisterInputFields" type="password" id="Password" placeholder="Password" required
                         name='password'
                         value={password}
                         onChange = {this.handleChange}
@@ -120,7 +147,7 @@ handleChange = (event) => {
                         <br/>
 
                         <label className="SignUp_ConfirmPassword">Confirm Password</label>
-                        <input className="LoginRegisterInputFields" type="password" id="ConfirmPassword" placeholder="Confirm Password" 
+                        <input className="LoginRegisterInputFields" type="password" id="ConfirmPassword" placeholder="Confirm Password" required
                         name='confirmpassword'
                         value={confirmpassword}
                         onChange = {this.handleChange}
@@ -135,6 +162,7 @@ handleChange = (event) => {
     );
   }
 }
+
  
 
 export default SignUpForm;
