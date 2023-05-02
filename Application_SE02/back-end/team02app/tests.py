5da74dd97dc866ff05406dcb489f2463ace817a6
Ashmitha <ashdale30@gmail.com>
2023-05-01 22:42:32 -0700

    Movie recommendation front-end and back-end updated tests

--- Application_SE02/back-end/team02app/tests.py
+++ Application_SE02/back-end/team02app/tests.py
@@ -1,51 +1,29 @@
-from django.test import TestCase, Client
-from django.urls import reverse, resolve
-from rest_framework.test import APIClient
-
-from team02app.views import index, RegisterAPI
-
-class TestURLs(TestCase):
-    
-    # Test if index url runs correct function
-    def test_index(self):
-        url = reverse('index')
-        self.assertEquals(resolve(url).func, index)
-
-    # Test if register url runs correct function.
-    def test_register(self):
-        url = reverse('register')
-        self.assertEquals(resolve(url).func.view_class, RegisterAPI)
-
-class TestViews(TestCase):
-    
-    # Test if posting data to register url correctly returns 
-    # succesfully and with correct data.
-    def test_register_POST(self):
-        client = Client()
-
-        response = client.post(reverse('register'), {
-            "email": "email@test.com",
-            "username": "test_username",
-            "password": "test_password"
-        })
-
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.data["user"]["username"], "test_username")
-       
-    # Test if posting no data to register url correctly returns 
-    # status code 400 (bad request).
-    def test_register_POST_no_data(self):
-        client = Client()
-        response = client.post(reverse('register'))
-
-        self.assertEquals(response.status_code, 400)
+from django.test import TestCase
+from django.urls import reverse
 
 
+class TestCases(TestCase):
+    fixtures = ['test_data_dump.json']
 
+    def setUp(self):
+        self.username = 'christest4'
+        self.password = 'millie1130'
 
+    def test_get_movie_rec_valid(self):
+        login_input_payload = {
+            "username": self.username,
+            "password": self.password
+        }
 
-        
-         
-        
-  
-        
+        login_response = self.client.post('/api/login/', data=login_input_payload)
+        print(login_response.json())
+
+        headers = {'HTTP_Authorization': 'Token ' + login_response.json()['token']}
+        print(headers)
+
+        reverse_url = reverse('get_movie_recommendations')
+        input_payload = {"genre": ["Adventure"], "year": ["1960 to 1980"], "runtime": ["120 to 150"], "age": ["PG-13"]}
+        response = self.client.post(reverse_url, input_payload, format='json',
+                                    HTTP_AUTHORIZATION='Token ' + login_response.json()['token'])
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
