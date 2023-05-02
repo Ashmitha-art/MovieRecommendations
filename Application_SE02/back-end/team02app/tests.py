b0c6b1b52908e22ccacf2286c13abaa01960c469
Ashmitha <ashdale30@gmail.com>
2023-05-01 23:06:36 -0700

    Updates test for login as well

--- Application_SE02/back-end/team02app/tests.py
+++ Application_SE02/back-end/team02app/tests.py
@@ -8,22 +8,33 @@ class TestCases(TestCase):
     def setUp(self):
         self.username = 'christest4'
         self.password = 'millie1130'
+        self.loginUser = 'christest54'
+        self.loginPass = 'millie11300'
 
-    def test_get_movie_rec_valid(self):
+    def test_login(self):
         login_input_payload = {
-            "username": self.username,
-            "password": self.password
+            "username": self.loginUser,
+            "password": self.loginPass
         }
 
         login_response = self.client.post('/api/login/', data=login_input_payload)
         print(login_response.json())
+        self.assertEqual(login_response.status_code, 400)
 
-        headers = {'HTTP_Authorization': 'Token ' + login_response.json()['token']}
-        print(headers)
+    def test_get_movie_rec_valid(self):
+        input_payload = {
+            "username": self.username,
+            "password": self.password
+        }
+
+        response = self.client.post('/api/login/', data=input_payload)
+        print(response.json())
+
+        headers = {'HTTP_Authorization': 'Token ' + response.json()['token']}
 
         reverse_url = reverse('get_movie_recommendations')
         input_payload = {"genre": ["Adventure"], "year": ["1960 to 1980"], "runtime": ["120 to 150"], "age": ["PG-13"]}
         response = self.client.post(reverse_url, input_payload, format='json',
-                                    HTTP_AUTHORIZATION='Token ' + login_response.json()['token'])
+                                    HTTP_AUTHORIZATION='Token ' + response.json()['token'])
         print(response.json())
         self.assertEqual(response.status_code, 200)
