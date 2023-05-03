277ead113cb9992a581e30ff8910170f39bc6c4b
NathanLoo4042 <nathanloo4042@gmail.com>
2023-05-02 23:13:02 -0700

    Manually integrated changes from testing_movieRecommendations

--- Application_SE02/back-end/team02app/tests.py
+++ Application_SE02/back-end/team02app/tests.py
@@ -40,7 +40,39 @@ class TestViews(TestCase):
 
         self.assertEquals(response.status_code, 400)
 
+class TestCases(TestCase):
+    fixtures = ['test_data_dump.json']
 
+    def setUp(self):
+        self.username = 'christest4'
+        self.password = 'millie1130'
+        self.loginUser = 'christest54'
+        self.loginPass = 'millie11300'
+
+    def test_login(self):
+        login_input_payload = {
+            "username": self.loginUser,
+            "password": self.loginPass
+        }
+
+        login_response = self.client.post('/api/login/', data=login_input_payload)
+        self.assertEqual(login_response.status_code, 400)
+
+    def test_get_movie_rec_valid(self):
+        input_payload = {
+            "username": self.username,
+            "password": self.password
+        }
+
+        response = self.client.post('/api/login/', data=input_payload)
+
+        headers = {'HTTP_Authorization': 'Token ' + response.json()['token']}
+
+        reverse_url = reverse('get_movie_recommendations')
+        input_payload = {"genre": ["Adventure"], "year": ["2000 to 2020"], "runtime": ["120 to 150"], "age": ["PG-13"]}
+        response = self.client.post(reverse_url, input_payload, format='json',
+                                    HTTP_AUTHORIZATION='Token ' + response.json()['token'])
+        self.assertEqual(response.status_code, 200)
 
 
 
